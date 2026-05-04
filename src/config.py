83b7744d84edf6b3efe02ba2e43a64d14fdf210d
danielliwag <danielarylliwag@gmail.com>
2026-05-05 00:00:56 +0800

    added database config

---
+++ src/config.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_access_token():
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+
+    # Token request
+    token_url = 'https://accounts.spotify.com/api/token'
+    data = {'grant_type': 'client_credentials'}
+
+    response = requests.post(token_url, auth=(client_id, client_secret), data=data)
+    access_token = response.json().get('access_token')
+    return access_token
+
+def get_db_config():
+      return {
+        "host": os.getenv("DB_HOST"),
+        "dbname": os.getenv("DB_NAME"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASS")
+    }
