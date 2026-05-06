3e699d88007804ed9dfd1e31b20816d685b83290
danielliwag <danielarylliwag@gmail.com>
2026-05-06 19:26:31 +0800

    updated the OAuth2 token to use spotipy library

--- src/config.py
+++ src/config.py
@@ -1,20 +1,18 @@
 import os
 import requests
+import spotipy
+from spotify.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_access_token():
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
-
-    # Token request
-    token_url = 'https://accounts.spotify.com/api/token'
-    data = {'grant_type': 'client_credentials'}
-
-    response = requests.post(token_url, auth=(client_id, client_secret), data=data)
-    access_token = response.json().get('access_token')
-    return access_token
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv('CLIENT_ID'),
+        client_secret=os.getenv('CLIENT_SECRET'),
+        redirect_uri= os.getenv('REDIRECT_URI'),
+        scope="user-read-recently-played"
+    ))
 
 def get_db_config():
       return {
